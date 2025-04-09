aa2c1bd52685bb83bcba5f234ac0bce622aa43f4
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-04-09 16:40:14 +0530

    adding more file

--- src/db/index.js
+++ src/db/index.js
@@ -4,9 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
-  } catch (error) {
+  } catch (err) {
     console.error("Mongodb connection failed", error);
-    process.exit(1);
+    process.exit(1); // Exit if connection fails
   }
 };
 
